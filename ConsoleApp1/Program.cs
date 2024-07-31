c13ad7cfe2e1d2b858c8781e0940936f03bf67fb
Lenovo <Lenovo@DESKTOP-HQH9Q0V>
2024-07-31 12:01:17 -0400

    A

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el primer número: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese el segundo número: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        if (num2 != 0)
+        {
+            double result = num1 / num2;
+            Console.WriteLine("El resultado de la división es: " + result);
+        }
+        else
+        {
+            Console.WriteLine("No se puede dividir por cero.");
+        }
+    }
+}
